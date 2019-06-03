89da22a24f1de946c15a2a8accc46444cb6a0beb
Lior Gingihashvili <lgingi@yotpo.com>
2019-06-03 19:46:12 +0300

    Init FXML

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -1,5 +1,39 @@
 package view;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MainWindowController {
+	@FXML
+	private Button backToMain;
+	@FXML
+	private Button connectServerBtn;
+	@FXML
+	private Button openConnectWindow;
 	
+	@FXML
+	private void handleConnectWindow(ActionEvent event) throws IOException {
+		Stage stage;
+		Parent root;
+		
+		if(event.getSource()==openConnectWindow) {
+			stage=(Stage)openConnectWindow.getScene().getWindow();
+			root=FXMLLoader.load(getClass().getResource("ConnectPopup.fxml"));
+		}else {
+			stage=(Stage)backToMain.getScene().getWindow();
+			root=FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+		}
+		
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+			
+	}
 }
