20a702b8008112db5a747cd32bbc7ea5489cc362
Lior Gingihashvili <lgingi@yotpo.com>
2019-06-10 21:19:03 +0300

    Joystick
    *Create and set Joystick for GUI

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -2,22 +2,29 @@ package view;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.lang.Math;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MainWindowController {
+	// local variables for joystick position
+	double orgSceneX, orgSceneY;
+	double orgTranslateX, orgTranslateY;
+
 	@FXML
 	private Button backToMain;
 	@FXML
@@ -26,24 +33,28 @@ public class MainWindowController {
 	private Button openConnectWindow;
 	@FXML
 	private GroundConditionDisplayer groundConditionDisplayer;
-	
+	@FXML
+	private Circle joystick;
+	@FXML
+	private Circle frameCircle;
+
 	@FXML
 	private void handleConnectWindow(ActionEvent event) throws IOException {
 		Stage stage;
 		Parent root;
-		
-		if(event.getSource()==openConnectWindow) {
-			stage=(Stage)openConnectWindow.getScene().getWindow();
-			root=FXMLLoader.load(getClass().getResource("ConnectPopup.fxml"));
-		}else {
-			stage=(Stage)backToMain.getScene().getWindow();
-			root=FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+
+		if (event.getSource() == openConnectWindow) {
+			stage = (Stage) openConnectWindow.getScene().getWindow();
+			root = FXMLLoader.load(getClass().getResource("ConnectPopup.fxml"));
+		} else {
+			stage = (Stage) backToMain.getScene().getWindow();
+			root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
 		}
-		
+
 		stage.setScene(new Scene(root));
 		stage.show();
 	}
-	
+
 	@FXML
 	private void loadData() {
 		FileChooser fc = new FileChooser();
@@ -57,7 +68,7 @@ public class MainWindowController {
 			ArrayList<String[]> valuesTable = new ArrayList<>();
 			String[] currentRow;
 			double[][] valuesInDouble = null;
-			
+
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(file));
 				while ((line = br.readLine()) != null) {
@@ -65,12 +76,12 @@ public class MainWindowController {
 				}
 				int rowSize = valuesTable.get(0).length;
 				valuesInDouble = new double[valuesTable.size()][rowSize];
-				for (int i=0; i<valuesTable.size(); i++) {
+				for (int i = 0; i < valuesTable.size(); i++) {
 					currentRow = valuesTable.get(i);
-					for(int j=0; j<rowSize; j++) {
+					for (int j = 0; j < rowSize; j++) {
 						double currentVal = Double.parseDouble(currentRow[j]);
 						valuesInDouble[i][j] = currentVal;
-						if(min == null || currentVal < min) {
+						if (min == null || currentVal < min) {
 							min = currentVal;
 						}
 						if (max == null || currentVal > max) {
@@ -82,15 +93,62 @@ public class MainWindowController {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 			HashMap<String, double[][]> retValues = new HashMap<>();
 			double[][] minMax = new double[1][2];
 			minMax[0][0] = min;
 			minMax[0][1] = max;
-			retValues.put("table", valuesInDouble);;
+			retValues.put("table", valuesInDouble);
+			;
 			retValues.put("minMax", minMax);
 			groundConditionDisplayer.setGroundField(retValues);
 		}
+
+	}
+
+	@FXML
+	private void joystickPressed(MouseEvent t) {
+//		EventHandler<MouseEvent> joystickPressedHandler = (MouseEvent t) -> {
+		orgSceneX = t.getSceneX();
+		orgSceneY = t.getSceneY();
+		orgTranslateX = ((Circle) (t.getSource())).getTranslateX();
+		orgTranslateY = ((Circle) (t.getSource())).getTranslateY();
+//	};
+	}
+
+	@FXML
+	private void joystickDragged(MouseEvent t) {
+		double offsetX = t.getSceneX() - orgSceneX;
+		double offsetY = t.getSceneY() - orgSceneY;
+		double newTranslateX = orgTranslateX + offsetX;
+		double newTranslateY = orgTranslateY + offsetY;
+		double joystickCenterX = frameCircle.getTranslateX() + frameCircle.getRadius() - joystick.getRadius();
+		double joystickCenterY = frameCircle.getTranslateY() - frameCircle.getRadius() - joystick.getRadius();
+		double frameRadius = frameCircle.getRadius();
+
+		double slant = Math.sqrt(Math.pow(newTranslateX-joystickCenterX, 2) + Math.pow(newTranslateY-joystickCenterY, 2));
 		
+		if(slant > frameRadius) {
+			double alpha=Math.atan((newTranslateY-joystickCenterY)/(newTranslateX-joystickCenterX));
+			if((newTranslateX-joystickCenterX)<0) {
+				alpha=alpha+Math.PI;
+			}	
+			double newX=Math.cos(alpha)*frameRadius + orgTranslateX;
+			double newY=Math.sin(alpha)*frameRadius + orgTranslateY;
+			((Circle) (t.getSource())).setTranslateX(newX);
+			((Circle) (t.getSource())).setTranslateY(newY);
+			System.out.println("x: "+ newX+ "  y: "+ newY);
+		} else {
+			((Circle) (t.getSource())).setTranslateX(newTranslateX);
+			((Circle) (t.getSource())).setTranslateY(newTranslateY);
+		}
+		
+
+	}
+	
+	@FXML
+	private void joystickReleased(MouseEvent t) {
+		((Circle) (t.getSource())).setTranslateX(frameCircle.getTranslateX() + frameCircle.getRadius() - joystick.getRadius());
+		((Circle) (t.getSource())).setTranslateY(frameCircle.getTranslateY() - frameCircle.getRadius() - joystick.getRadius());
 	}
 }
